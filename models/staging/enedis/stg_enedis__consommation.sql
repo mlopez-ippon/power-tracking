3c104898edb598950c22e02f4855693b10d965b7
mlopez@ippon.fr <mlopez@ippon.fr>
2023-04-05 08:28:49 +0000

    Différence ajoutée sur chaque colonne énergie

--- models/staging/enedis/stg_enedis__consommation.sql
+++ models/staging/enedis/stg_enedis__consommation.sql
@@ -6,13 +6,13 @@ with enedis_consommation as (
     select 
         conso_date
         , city
-        , h_offpeak_supplier
-        , h_peak_supplier
+        , ifnull(h_offpeak_supplier,0)              as h_offpeak_supplier
+        , ifnull(h_peak_supplier,0)                 as h_peak_supplier
         , h_offpeak_low_season_distributor
         , h_peak_low_season_distributor
         , h_offpeak_high_season_distributor
         , h_peak_high_season_distributor
-        , total_sum
+        , ifnull(total_sum,0)                       as total_sum
     from 
         enedis_consommation
     qualify
