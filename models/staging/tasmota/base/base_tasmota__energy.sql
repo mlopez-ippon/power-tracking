fd0bdd6eb22c9543e5735227638ebd237c2da58d
mlegall@ippon.fr <mlegall@ippon.fr>
2023-06-07 13:22:07 +0000

    correction input base tasmota

--- models/staging/tasmota/base/base_tasmota__energy.sql
+++ models/staging/tasmota/base/base_tasmota__energy.sql
@@ -1,5 +1,5 @@
 with source as (
-    {{ mockable_source('input_meteo','FEATURE_SILVER','FCT_TASMOTA_ENERGY') }}
+    {{ mockable_source('input_tasmota','FEATURE_SILVER','FCT_TASMOTA_ENERGY') }}
 ) 
 
 select * from source
